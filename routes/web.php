27655618c52df3d51cea12e2d1ab9ea653e11b38
vignesh <92292214+vkytnvemart@users.noreply.github.com>
2025-07-26 12:59:27 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TimeLogController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/time-logs', [TimeLogController::class, 'index'])->name('time_logs.index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [TimeLogController::class, 'index'])->name('time_logs.index');
 Route::post('/time-logs', [TimeLogController::class, 'store'])->name('time_logs.store');
 Route::get('/time-logs/{timeLog}/edit', [TimeLogController::class, 'edit'])->name('time_logs.edit');
 Route::put('/time-logs/{timeLog}', [TimeLogController::class, 'update'])->name('time_logs.update');
